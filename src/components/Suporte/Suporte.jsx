34e91bacfd5bd5416279cfec37380e37f3139c03
Alberth Nery <107318708+asmnery@users.noreply.github.com>
2025-11-14 23:30:49 -0300

    feat: add componente 'AttFirmware' na suporte

--- src/components/Suporte/Suporte.jsx
+++ src/components/Suporte/Suporte.jsx
@@ -2,6 +2,7 @@ import style from "./Suporte.module.css";
 import HeroSuporte from "./HeroSuporte/HeroSuporte";
 import GuiasUso from "./GuiasUso/GuiasUso";
 import CentralAjuda from "./CentralAjuda/CentralAjuda";
+import AttFirmware from "./AttFirmware/AttFirmware";
 
 export default function Suporte() {
   return (
@@ -9,6 +10,7 @@ export default function Suporte() {
         <HeroSuporte />
         <GuiasUso />
         <CentralAjuda />
+        <AttFirmware />
     </main>
   );
 }
