4c61ed7fd80d905c85f2f738d1a8d0bc7b856fa9
Alberth Nery <iophyhd@gmail.com>
2025-11-19 17:14:24 -0300

    feat: add componente 'Design' e 'SecaoComponentes' na especproduto'

--- src/components/EspecProduto/EspecProduto.jsx
+++ src/components/EspecProduto/EspecProduto.jsx
@@ -1,9 +1,13 @@
 import HeroEspecProduto from "./HeroEspecProduto/HeroEspecProduto";
+import Design from "./Design/Design";
+import SecaoComponentes from "./SecaoComponentes/SecaoComponentes";
 
 export default function EspecProduto() {
   return (
     <main>
       <HeroEspecProduto />
+      <Design />
+      <SecaoComponentes />
     </main>
   );
 }
